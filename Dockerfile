8e690837e4f38f21b6acfd6ba7c59316626209d5
Plixelated <76276169+Plixelated@users.noreply.github.com>
2024-07-20 14:24:01 -0700

    Fixed Issues with entrypoint.sh not running python script, updated Dockerfile and removed typo
    - Fixed Issues with entrypoint.sh not running python script
    - Updated Dockerfile and removed CHMOD typo

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9-slim
 WORKDIR /app
 RUN apt-get update && apt-get install -y git
 COPY entrypoint.sh /app/entrypoint.sh
-RUN CHMOD +x /app/entrypoint.sh
+RUN chmod +x /app/entrypoint.sh
 COPY requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
-git clone https://github.com/Plixelated/Roll-Bot.git || (cd Roll-Bot && git checkout docker && git pull origin docker)
+#!/bin/bash
+
+if [ ! -d "Roll-Bot" ]; then
+    git clone https://github.com/Plixelated/Roll-Bot.git 
+fi
+
 cd Roll-Bot
+
+git checkout docker 
+git pull origin docker
+
+if [ ! -f "$DISCORD_TOKEN_FILE" ]; then
+    echo "Token file not found: $DISCORD_TOKEN_FILE"
+    exit 1
+fi
 
 DISCORD_TOKEN=$(cat $DISCORD_TOKEN_FILE)
 
 export DISCORD_TOKEN
 
-python /bot/main.py
+python /app/Roll-Bot/bot/main.py
